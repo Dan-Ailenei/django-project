a92fd883bfe8990e2c766ec7b149dd1ef554a56a
Ailenei Dan <dan.ailenei@rover.com>
2019-12-21 11:38:09 +0200

    fix

--- {{ cookiecutter.project_name }}/src/{{ cookiecutter.project_name }}_auth/models.py
+++ {{ cookiecutter.project_name }}/src/{{ cookiecutter.project_name }}_auth/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class {{ cookiecutter.project_name|replace('_', ' ')|title|replace(' ', '') }}User(AbstractUser):
