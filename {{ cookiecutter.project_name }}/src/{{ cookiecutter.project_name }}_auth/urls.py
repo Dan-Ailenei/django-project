0f1d83fb242bd7a4b3c53677b361d9b5c1636e53
Ailenei Dan <dan.ailenei@rover.com>
2019-12-21 13:39:57 +0200

    template for login

--- {{ cookiecutter.project_name }}/src/{{ cookiecutter.project_name }}_auth/urls.py
+++ {{ cookiecutter.project_name }}/src/{{ cookiecutter.project_name }}_auth/urls.py
@@ -4,6 +4,9 @@ from django.urls import path
 app_name = "{{ cookiecutter.project_name }}_auth"
 
 urlpatterns = [
-    path('login/', LoginView.as_view(redirect_authenticated_user=True), name='login'),
+    path('login/',
+          LoginView.as_view(redirect_authenticated_user=True,
+                           template_name="{{ cookiecutter.project_name }}_auth/login.html"),
+          name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
